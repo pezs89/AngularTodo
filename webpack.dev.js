b3f454020e280ecd5b56602e1019b15c0bf5386e
Peter <zsolt.peter@nokia.com>
2018-06-13 13:09:33 +0200

    font awesome with webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -50,7 +50,7 @@ const config = {
             },
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+                loader: 'file-loader?name=assets/[name].[ext]'
             }, {
                 test: /\.(css|scss|sass)$/,
                 use: ExtractTextPlugin.extract({
